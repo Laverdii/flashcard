b3e55516b95d1c64c3071252fe97c3355f4c2628
Laverdii <132484302+Laverdii@users.noreply.github.com>
2024-11-07 10:57:42 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'Videogames',
+    'O que é Minecraft?',
+    
+    'O Minecraft é um jogo de mundo aberto e exploração'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasilia'
+)
+
+criaCartao(
+    'Programação',
+    'O que é o VS Code?',
+    'Um aplicativo usado para programar'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz Deus te abençoe em Inglês?',
+    'God bless you'
+)
